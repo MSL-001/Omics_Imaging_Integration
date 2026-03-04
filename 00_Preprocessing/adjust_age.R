ad2ee673fc9d8dda80836f91c00d1f5a2a605d5b
MSL-001 <121134595+MSL-001@users.noreply.github.com>
2026-03-04 14:20:31 +0100

    small script to add age between instance 0 and instance 2

---
+++ 00_Preprocessing/adjust_age.R
@@ -0,0 +1,8 @@
+age_data <- read.csv("age_data.csv")
+
+
+age_data <- na.omit(age_data)
+
+age_data$age_between <- age_data$age_2 - age_data$age_0
+
+write.csv(age_data, "age_data_new.csv", row.names = F)
