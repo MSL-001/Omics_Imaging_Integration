97e2bf735b6c2c09a7ce2cbaa70e5536f52d200c
MSL-001 <121134595+MSL-001@users.noreply.github.com>
2026-03-24 11:11:47 +0100

    Added support for height correction and different correlations

--- 01_Statistics/generate_heatmap.R
+++ 01_Statistics/generate_heatmap.R
@@ -3,35 +3,42 @@ library(pheatmap)
 library(dplyr)
 library(tidyr)
 
-generate_heatmap <- function(title, cohort, file, generate_cluster, scale, rows=F,remove_prostate=F){
+generate_heatmap <- function(title, cohort, file, corr_type, generate_cluster=F, scale=F, rows=F,remove_prostate=F,panel=F){
   data_folder <- "Data/01_Statistics/"
 
   file_path <- paste0(data_folder, cohort, file)
 
   file_path_encoding <- paste0("Data/", "Metadata/encoding.csv")
 
-  encoding <<- read.csv(file_path_encoding)
+  encoding <- read.csv(file_path_encoding)
 
   results <- read.csv(file_path)
 
-  #significant_results <- subset(results, Rows_used > 500 &  q_value < 0.01)
+  Adj_corr <- paste0("Adjusted_corr_", corr_type)
+  q_value_col <- paste0("q_value_", corr_type)
 
-  #filtered_results <- subset(significant_results, abs(Adjusted_corr) > 0)
+  if (!identical(panel, F)) {
+    # keep_panels <- c(panel, paste(panel, "II"))
+    keep_panels <- c(panel)
+    keep_codes <- encoding$Code[encoding$Panel %in% keep_panels]
+    results <- results %>% dplyr::filter(Omics_Feature %in% keep_codes)
+  }
 
-  #omic_keep <- unique(filtered_results$Omics_Feature)
-  #feature_keep <- unique(filtered_results$BC_feature)
+  # significant_results <- subset(results, Rows_used > 500 &  q_value_col < 0.01)
+  #
+  # filtered_results <- subset(significant_results, abs(Adj_corr) > 0)
+  #
+  # omic_keep <- unique(filtered_results$Omics_Feature)
+  # feature_keep <- unique(filtered_results$BC_feature)
+  #
+  # results <- results %>%
+  #   dplyr::filter(Omics_Feature %in% omic_keep)
 
-  #significant_results <- significant_results %>%
-    #dplyr::filter(Omics_Feature %in% omic_keep)
-
-  #significant_results <- significant_results %>%
-  #  dplyr::filter(BC_feature %in% feature_keep)
-
-  results$Adjusted_corr[!(results$Rows_used > 1000 & results$q_value < 0.01)] <- NA
+  results[[Adj_corr]][!(results$Rows_used > 1000 & results[[q_value_col]] < 0.01)] <- NA
 
   mat_df <- results %>%
-    dplyr::select(Omics_Feature, BC_feature, Adjusted_corr) %>%
-    tidyr::pivot_wider(names_from = BC_feature, values_from = Adjusted_corr)
+    dplyr::select(Omics_Feature, BC_feature, all_of(Adj_corr)) %>%
+    tidyr::pivot_wider(names_from = BC_feature, values_from = all_of(Adj_corr))
 
   if (!"cardiac_fat" %in% colnames(mat_df)) {
   mat_df[["cardiac_fat"]] <- NA
@@ -97,28 +104,54 @@ generate_heatmap <- function(title, cohort, file, generate_cluster, scale, rows=
 }
 
 rows <- F
-cohort <- "Met_m/"
 scale <- c(-0.4, 0.4)
-generate_heatmap("Metabolomics Male robust meanff vibe100", cohort, "corr_adj_robustmeanff_100.csv", scale, generate_cluster = T, rows=rows)
-generate_heatmap("Metabolomics Male robust meanff vibe100_processed", cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Metabolomics Male volume vibe100", cohort, "corr_adj_vol_100.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Metabolomics Male volume vibe100_processed", cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = F, rows=rows)
+cohort <- "Met_m/"
+
+for (feature_type in c("robustmeanff_100_processed", "vol_100_processed")){
+  clusters <- T
+  for (corr_type in c("pearson_height", "pearson", "spearman", "spearman_height")){
+      generate_heatmap( paste0("Metabolomics Male ", feature_type,"_",corr_type), cohort, paste0("corr_adj_",feature_type,".csv"), corr_type, scale=scale,  generate_cluster = clusters, rows=rows)
+        clusters <- F
+  }
+}
 
 cohort <- "Met_f/"
-generate_heatmap("Metabolomics Female robust meanff vibe100", cohort, "corr_adj_robustmeanff_100.csv", scale, generate_cluster = T, rows=rows)
-generate_heatmap("Metabolomics Female robust meanff vibe100_processed", cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Metabolomics Female volume vibe100", cohort, "corr_adj_vol_100.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Metabolomics Female volume vibe100_processed", cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = F, rows=rows)
-
+for (feature_type in c("robustmeanff_100_processed", "vol_100_processed")){
+  clusters <- T
+  for (corr_type in c("pearson_height", "pearson", "spearman", "spearman_height")){
+      generate_heatmap( paste0("Metabolomics Female ", feature_type,"_",corr_type), cohort, paste0("corr_adj_",feature_type,".csv"), corr_type, scale=scale,  generate_cluster = clusters, rows=rows)
+        clusters <- F
+  }
+}
 scale <- c(-0.4, 0.4)
+# for (panel in c("Oncology", "Neurology", "Cardiometabolic", "Inflammation","Oncology II", "Neurology II", "Cardiometabolic II", "Inflammation II")){
+#   cohort <- "Single_prot_m/"
+#   generate_heatmap(paste("Proteomics Male robust meanff vibe100_processed", panel), cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = T, rows=rows, panel=panel)
+#   generate_heatmap(paste("Proteomics Male volume vibe100_processed", panel), cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = T, rows=rows, panel=panel)
+# }
 cohort <- "Single_prot_m/"
-generate_heatmap("Proteomics Male robust meanff vibe100", cohort, "corr_adj_robustmeanff_100.csv", scale, generate_cluster = T, rows=rows)
-generate_heatmap("Proteomics Male robust meanff vibe100_processed", cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Proteomics Male volume vibe100", cohort, "corr_adj_vol_100.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Proteomics Male volume vibe100_processed", cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = F, rows=rows)
+
+for (feature_type in c("robustmeanff_100_processed", "vol_100_processed")){
+  clusters <- T
+  for (corr_type in c("pearson_height", "pearson", "spearman", "spearman_height")){
+      generate_heatmap(paste0("Proteomics Male ", feature_type,"_",corr_type), cohort, paste0("corr_adj_",feature_type,".csv"), corr_type, scale=scale,  generate_cluster = clusters, rows=rows)
+        clusters <- F
+  }
+}
+
+
+# for (panel in c("Oncology", "Neurology", "Cardiometabolic", "Inflammation","Oncology II", "Neurology II", "Cardiometabolic II", "Inflammation II")){
+#   cohort <- "Single_prot_f/"
+#   generate_heatmap(paste("Proteomics Female robust meanff vibe100_processed", panel), cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = T, rows=rows, panel=panel)
+#   generate_heatmap(paste("Proteomics Female volume vibe100_processed", panel), cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = T, rows=rows, panel=panel)
+# }
 
 cohort <- "Single_prot_f/"
-generate_heatmap("Proteomics Female robust meanff vibe100", cohort, "corr_adj_robustmeanff_100.csv", scale, generate_cluster = T, rows=rows)
-generate_heatmap("Proteomics Female robust meanff vibe100_processed", cohort, "corr_adj_robustmeanff_100_processed.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Proteomics Female volume vibe100", cohort, "corr_adj_vol_100.csv", scale, generate_cluster = F, rows=rows)
-generate_heatmap("Proteomics Female volume vibe100_processed", cohort, "corr_adj_vol_100_processed.csv", scale, generate_cluster = F, rows=rows)
+
+for (feature_type in c("robustmeanff_100_processed", "vol_100_processed")){
+  clusters <- T
+  for (corr_type in c("pearson_height", "pearson", "spearman", "spearman_height")){
+      generate_heatmap(paste0("Proteomics Female ", feature_type,"_",corr_type), cohort, paste0("corr_adj_",feature_type,".csv"), corr_type, scale=scale,  generate_cluster = clusters, rows=rows)
+        clusters <- F
+  }
+}
